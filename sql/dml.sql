0081f9f8633a23c5935b3a9e30ffbf3453f10e8d
KevinKing66 <kevinvallenato2002@gmail.com>
2023-07-13 21:04:02 -0500

    se actualizaa dml

--- sql/dml.sql
+++ sql/dml.sql
@@ -8,27 +8,27 @@ INSERT INTO roles (id, name) VALUES (1, 'ADMIN');
 INSERT INTO roles (id, name) VALUES (2, 'INSPECTOR');
 INSERT INTO roles (id, name) VALUES (3, 'SELLER');
 
-INSERT INTO users (id, email, name, role_id, role, password, Salt) 
-VALUES (1, 'example@email.com', 'Kevin', 1, 'admin', 'password', 'somesalt');
+INSERT INTO users (id, email, name, role_id, role, password, status) 
+VALUES (9, 'Kevin@email.com', 'Kevin', 1, 'ADMIN', 'password', 'ENABLE');
 
 
 --lotteries--
-INSERT INTO lotteries VALUES('CUND', 'CUNDINAMARCA',1, '22:30:00');
-INSERT INTO lotteries VALUES('TOLI', 'TOLIMA',1, '23:00:00');
+INSERT INTO lotteries VALUES('CUND', 1, 'CUNDINAMARCA', '22:30:00');
+INSERT INTO lotteries VALUES('TOLI', 1, 'TOLIMA', '23:00:00');
 
-INSERT INTO lotteries VALUES('CRUZ', 'CRUZ ROJA',2, '22:30:00');
-INSERT INTO lotteries VALUES('HUIL', 'HUILA',2, '22:30:00');
+INSERT INTO lotteries VALUES('CRUZ', 2, 'CRUZ ROJA', '22:30:00');
+INSERT INTO lotteries VALUES('HUIL', 2, 'HUILA', '22:30:00');
 
-INSERT INTO lotteries VALUES('CUND', 'MANIZALES',3, '22:30:00');
-INSERT INTO lotteries VALUES('META', 'META',3, '22:30:00');
-INSERT INTO lotteries VALUES('VALL', 'VALLE',3, '22:30:00');
+INSERT INTO lotteries VALUES('CUND', 3, 'MANIZALES', '22:30:00');
+INSERT INTO lotteries VALUES('META', 3, 'META', '22:30:00');
+INSERT INTO lotteries VALUES('VALL', 3, 'VALLE', '22:30:00');
 
-INSERT INTO lotteries VALUES('QUIN', 'QUINDÍO',4, '22:30:00');
-INSERT INTO lotteries VALUES('BOGO', 'BOGOTÁ',4, '22:30:00');
+INSERT INTO lotteries VALUES('QUIN', 4, 'QUINDÍO', '22:30:00');
+INSERT INTO lotteries VALUES('BOGO', 4, 'BOGOTÁ', '22:30:00');
 
-INSERT INTO lotteries VALUES('CUND', 'SORTEO SANTANDER',5, '23:00:00');
-INSERT INTO lotteries VALUES('MEDE', 'MEDELLÍN',5, '23:00:00');
-INSERT INTO lotteries VALUES('RISA', 'RISARALDA',5, '23:00:00');
+INSERT INTO lotteries VALUES('CUND', 5, 'SORTEO SANTANDER', '23:00:00');
+INSERT INTO lotteries VALUES('MEDE', 5, 'MEDELLÍN', '23:00:00');
+INSERT INTO lotteries VALUES('RISA', 5, 'RISARALDA', '23:00:00');
 
-INSERT INTO lotteries VALUES('BOYACÁ', 'CUNDINAMARCA',6, '22:40:00');
-INSERT INTO lotteries VALUES('CAUCA', 'CUNDINAMARCA',6, '21:40:00');
+INSERT INTO lotteries VALUES('BOYACÁ', 6, 'CUNDINAMARCA', '22:40:00');
+INSERT INTO lotteries VALUES('CAUCA', 6, 'CUNDINAMARCA', '21:40:00');
