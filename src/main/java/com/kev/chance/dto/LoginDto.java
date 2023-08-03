2e72c03f59cd32c4e622f47b5336d0c945c3f09d
KevinKing66 <kevinvallenato2002@gmail.com>
2023-08-03 16:28:55 -0500

    se quita validacion

--- src/main/java/com/kev/chance/dto/LoginDto.java
+++ src/main/java/com/kev/chance/dto/LoginDto.java
@@ -1,6 +1,5 @@
 package com.kev.chance.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +13,6 @@ import lombok.NoArgsConstructor;
 public class LoginDto {
 
     @NotEmpty(message = "El campo de correo electrónico es obligatorio")
-    @Email(message = "Proporciona un correo electrónico válido")
     private String email;
 
     @NotEmpty(message = "El campo de contraseña es obligatorio")
